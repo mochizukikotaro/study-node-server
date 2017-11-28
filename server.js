4e61763d61fea64c202bb6e02db7e4d0435c6bf2
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-11-28 13:05:05 +0900

    Enable text/css

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const path = require('path')
 const url = require('url')
 const fs = require("fs")
 const port = process.env.PORT || 1339
+const mime = {
+		".html": "text/html",
+		".css": "text/css"
+}
 
 const server = http.createServer()
 
 server.on('request', (request, response) => {
+
 
 	var uri = url.parse(request.url).pathname
 	var filename = path.join(process.cwd(), uri)
@@ -16,7 +21,11 @@ server.on('request', (request, response) => {
 		if (fs.statSync(filename).isDirectory()) { filename += '/index.html'; }
 
 		fs.readFile(filename, "utf-8", function(err, file){
-			response.writeHead(200, {'Content-Type': 'text/html'});
+
+			// これがないと css が読みなかったりする
+			const extname = path.extname(filename);
+
+			response.writeHead(200, {'Content-Type': mime[extname]});
 			response.write(file);
 			response.end();
 		});
